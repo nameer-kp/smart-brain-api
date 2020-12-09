e590817ac07bccfa1f726e8fc6e14dc6aadd6c46
Nameer-kp <kpnameer@yahoo.com>
2020-12-10 00:04:15 +0530

    database connection fix2

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const db =knex({
     connection: {
       hconnectionString: process.env.DATABASE_URL,
       ssl: {
-        rejectUnauthorized: false
+        rejectUnauthorized: true
       }
     }
   });
